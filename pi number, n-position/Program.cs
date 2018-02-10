45d8e9829acb0f8faa9f97276265c212e3121065
Mishen <mishen888@yandex.ru>
2018-02-10 15:37:03 +0300

    Initial commit

---
+++ pi number, n-position/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pi_number__n_position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number ;
+            double p = 0, i = 1;
+            for (int j = 1; j <= 9999999; j++)
+            {
+                if (j > 1)
+                {
+                    i += 2;
+                }
+                if (j % 2 == 1)
+                {
+                    p = p + (4 / i);
+                }
+                else
+                {
+                    p = p - (4 / i);
+                }
+                
+            }
+            Console.WriteLine("Insert pi number");
+            number = Convert.ToInt32(Console.ReadLine());
+            String value = p.ToString();
+            int startIndex = number+1;
+            int length = 1;
+            String substring = value.Substring(startIndex, length);
+            Console.WriteLine(substring);
+            //are working
+        }
+    }
+}
